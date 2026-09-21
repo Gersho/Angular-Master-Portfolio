af778a450b8d2e684b52905eb689e1e57bb6e490
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-09-21 16:22:36 +0200

    (hotfix)syntax fix

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
+
+  # --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`) || Host(`www.zennoune.fr`) || Host(`zennoune.fr`)' \
+
 docker run -d \
   --name my-portfolio \
   --network traefik-network \
   --label 'traefik.enable=true' \
   --label 'traefik.docker.network=traefik-network' \
-  # --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`) || Host(`www.zennoune.fr`) || Host(`zennoune.fr`)' \
-    --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`)' \
+  --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`)' \
   --label 'traefik.http.routers.portfolio-frontend.entrypoints=websecure' \
   --label 'traefik.http.routers.portfolio-frontend.tls.certresolver=letsencrypt' \
   --label 'traefik.http.services.portfolio-frontend.loadbalancer.server.port=80' \
