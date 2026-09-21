55d1748fbcc196fd48111cada4d6bed94bebe580
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-09-21 15:29:35 +0200

    updated run command

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ docker run -d \
   --name my-portfolio \
   --network traefik-network \
   --label "traefik.enable=true" \
-  --label "traefik.http.routers.frontend.rule=Host(\`test.zennoune.fr\`)" \
+  --label "traefik.docker.network=traefik-network" \
+  --label "traefik.http.routers.frontend.rule=Host('test.zennoune.fr') || Host('portfolio.zennoune.fr') || Host('www.portfolio.zennoune.fr') || Host('www.zennoune.fr') || Host('zennoune.fr')" \
   --label "traefik.http.routers.frontend.entrypoints=websecure" \
   --label "traefik.http.routers.frontend.tls.certresolver=letsencrypt" \
   --label "traefik.http.services.frontend.loadbalancer.server.port=80" \
