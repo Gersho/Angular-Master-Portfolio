3e7de558c8b9b961c15078cd4a21cbcb0a549f4c
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-09-21 16:18:57 +0200

    removing some addresses for portfolio access (temporary)

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ docker run -d \
   --network traefik-network \
   --label 'traefik.enable=true' \
   --label 'traefik.docker.network=traefik-network' \
-  --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`) || Host(`www.zennoune.fr`) || Host(`zennoune.fr`)' \
+  # --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`) || Host(`www.zennoune.fr`) || Host(`zennoune.fr`)' \
+    --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`)' \
   --label 'traefik.http.routers.portfolio-frontend.entrypoints=websecure' \
   --label 'traefik.http.routers.portfolio-frontend.tls.certresolver=letsencrypt' \
   --label 'traefik.http.services.portfolio-frontend.loadbalancer.server.port=80' \
