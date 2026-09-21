d7df78907ffd5e1b9b06fd0862bcb439c9995f4a
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-09-21 15:47:38 +0200

    change router and service name to avoid collision

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ docker run -d \
   --network traefik-network \
   --label "traefik.enable=true" \
   --label "traefik.docker.network=traefik-network" \
-  --label "traefik.http.routers.frontend.rule=Host('test.zennoune.fr') || Host('portfolio.zennoune.fr') || Host('www.portfolio.zennoune.fr') || Host('www.zennoune.fr') || Host('zennoune.fr')" \
-  --label "traefik.http.routers.frontend.entrypoints=websecure" \
-  --label "traefik.http.routers.frontend.tls.certresolver=letsencrypt" \
-  --label "traefik.http.services.frontend.loadbalancer.server.port=80" \
+  --label "traefik.http.routers.portfolio.rule=Host('test.zennoune.fr') || Host('portfolio.zennoune.fr') || Host('www.portfolio.zennoune.fr') || Host('www.zennoune.fr') || Host('zennoune.fr')" \
+  --label "traefik.http.routers.portfolio.entrypoints=websecure" \
+  --label "traefik.http.routers.portfolio.tls.certresolver=letsencrypt" \
+  --label "traefik.http.services.portfolio.loadbalancer.server.port=80" \
   portfolio:1.0
