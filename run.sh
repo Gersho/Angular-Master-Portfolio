ada7206e31e944ed8d51220b7bca1844249ce3fa
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-09-21 16:13:04 +0200

    changing run.sh to backticks again

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 docker run -d \
   --name my-portfolio \
   --network traefik-network \
-  --label "traefik.enable=true" \
-  --label "traefik.docker.network=traefik-network" \
-  --label "traefik.http.routers.portfolio.rule=Host('test.zennoune.fr') || Host('portfolio.zennoune.fr') || Host('www.portfolio.zennoune.fr') || Host('www.zennoune.fr') || Host('zennoune.fr')" \
-  --label "traefik.http.routers.portfolio.entrypoints=websecure" \
-  --label "traefik.http.routers.portfolio.tls.certresolver=letsencrypt" \
-  --label "traefik.http.services.portfolio.loadbalancer.server.port=80" \
+  --label 'traefik.enable=true' \
+  --label 'traefik.docker.network=traefik-network' \
+  --label 'traefik.http.routers.portfolio-frontend.rule=Host(`test.zennoune.fr`) || Host(`portfolio.zennoune.fr`) || Host(`www.zennoune.fr`) || Host(`zennoune.fr`)' \
+  --label 'traefik.http.routers.portfolio-frontend.entrypoints=websecure' \
+  --label 'traefik.http.routers.portfolio-frontend.tls.certresolver=letsencrypt' \
+  --label 'traefik.http.services.portfolio-frontend.loadbalancer.server.port=80' \
   portfolio:1.0
